cfcd1c83d33cc347ac6e40f24552ed7e495c827f
Miles <miles.moscara@gmail.com>
2019-05-25 21:49:12 -0400

    seed file and button addition

---
+++ scripts/seed.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+const products = require("./products.json");
+
+db.sequelize
+	.sync({ force: true })
+	.then(function() {
+		return db.Product.bulkCreate(products);
+	})
+	.then(function() {
+		db.sequelize.close();
+		console.log("planted seeds");
+	});
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var db = require("./models");
+const db = require("./models");
 
-var express = require("express");
+const express = require("express");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
