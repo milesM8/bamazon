3b1619eae762e140bc7b1578f2df8b4930a3aedd
Miles <miles.moscara@gmail.com>
2019-06-10 19:51:38 -0400

    running total working

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,41 @@
 $(document).ready(function () {
 
 	$(document).on("click", "button", addtoCart);
+	$(document).on("click", "button", add$);
 
-	function addtoCart(j) {
-		j.preventDefault();
+	function addtoCart() {
 		var name = $(this).closest("tr").children("#name").text();
-		var stock = $(this).closest("tr").children("#stockNum").text();
-		var price = $(this).closest("tr").children("#name").text();
+		// var stock = $(this).closest("tr").children("#stockNum").text();
+		var price = $(this).closest("tr").children("#price").text();
+		var chosenStock = $(this).closest("tr").find("#stock").val();
+		var priceTimesStock = (parseFloat(price) * chosenStock).toFixed(2);
+		// var numberPurchasing = $("#stock").val();
+		var tableBody = $("#cart");
+		var tableContainer = $(["<tr>",
+			"<th scope='row'></th>",
+			"<td id='name2'>", name, "</td>",
+			"<td id='stockNum2'>", chosenStock, "</td>",
+			"<td id='price2'>", priceTimesStock + "$", "</td>"].join(""));
+		tableBody.append(tableContainer);
+		$("#totalPrice").text(runningTotal(priceTimesStock));	
+		
+	}
 
+	function add$() {
+		var totalPrice = $("#totalPrice").text();
+		if (totalPrice != "") {
+			$("#totalPrice").append("$");
+		}
+	}
+	var runTotal = [];
+
+	function runningTotal(incomingPrice) {
+		let sum = 0;
+		runTotal.push(incomingPrice);
+		for (let i = 0; i < runTotal.length; i++) {
+			sum += parseFloat(runTotal[i]);
+		}
+		return sum;
 	}
 
 	function getProducts() {
@@ -24,7 +52,7 @@ $(document).ready(function () {
 				"<td id='department'>", data[i].department_name, "</td>",
 				"<td id='stockNum'>", data[i].stock_quantity, "</td>",
 				"<td id='price'>", data[i].price + "$", "</td>",
-				"<td>", "<div class='form-group'>", "<input type='number' class='form-control' id='stock'/>",
+				"<td id='stockChoice'>", "<div class='form-group'>", "<input type='number' class='form-control' id='stock'/>",
 				"</div>", "</td>", "<td>", "<button type='button' id='btn' class='btn btn-success'>", "<i class='fa fa-cart-plus'>", "</i>", "</button>", "</td>", "</tr>"
 			].join(""))
 			tableBody.append(tableContainer);
